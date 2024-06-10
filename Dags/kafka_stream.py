224b277ef3154309ec617df7b75dfc3689ebab7d
vamc526 <vamc526@gmail.com>
2024-06-09 21:18:41 -0400

    updated

--- Dags/kafka_stream.py
+++ Dags/kafka_stream.py
@@ -1,25 +1,21 @@
+import uuid
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
-
-
-
 
 default_args = {
-    'Owner': 'vamshi',
-    'start_date': datetime(2024, 5,17,23,00)
-    }
-
+    'owner': 'airscholar',
+    'start_date': datetime(2023, 9, 3, 10, 00)
+}
 
 def get_data():
     import requests
-    res=requests.get('https://randomuser.me/api/')
+
+    res = requests.get("https://randomuser.me/api/")
     res = res.json()
-    res= res['results'][0]
+    res = res['results'][0]
 
-    return(res)
-
+    return res
 
 def format_data(res):
     data = {}
@@ -40,5 +36,33 @@ def format_data(res):
 
     return data
 
+def stream_data():
+    import json
+    from kafka import KafkaProducer
+    import time
+    import logging
 
-stream_data(): 
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
+
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
+
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
